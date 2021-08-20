d19095499c2a428dd4795085710e30bf978e0fee
Ekaterina <88279403+EkaterinaT89@users.noreply.github.com>
2021-08-20 23:57:55 +0300

    new if else added

--- javabasicsdiploma/src/ru/netology/graphics/image/TextGraphicsConverterImpl.java
+++ javabasicsdiploma/src/ru/netology/graphics/image/TextGraphicsConverterImpl.java
@@ -8,18 +8,6 @@ import java.net.URL;
 
 public class TextGraphicsConverterImpl implements TextGraphicsConverter {
 
-    public String printNewPicture(char[][] picForPrint) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < picForPrint.length; i++) {
-            for (int j = 0; j < picForPrint[i].length; j++){
-                sb.append(picForPrint[i][j]);
-                sb.append(picForPrint[i][j]);
-            }
-            sb.append("\n");
-        }
-        return sb.toString();
-    }
-
     private int width;
     private int height;
     private double maxRatio;
@@ -28,16 +16,27 @@ public class TextGraphicsConverterImpl implements TextGraphicsConverter {
     @Override
     public String convert(String url) throws IOException, BadImageSizeException {
         double ratio = 0;
-        double widthToChange = 0;
-        double heightToChange = 0;
 
         int width = 200;
         int height = 300;
 
+        int newWidth = 0;
+        int newHeight = 0;
+
         BufferedImage img = ImageIO.read(new URL(url));
 
-        int newWidth = Math.max(width, img.getWidth());
-        int newHeight = Math.max(height, img.getHeight());
+        if (img.getHeight() > img.getWidth()) {
+            int widthForCount = img.getWidth() / width;
+            newHeight = img.getHeight() / widthForCount;
+            int heightForCount = img.getHeight() / height;
+            newWidth = img.getWidth() / heightForCount;
+
+        } else {
+            int widthForCount = img.getWidth() / height;
+            int heightForCount = img.getHeight() / width;
+            newHeight = img.getHeight() / heightForCount;
+            newWidth = img.getWidth() / widthForCount;
+        }
 
         if (img.getWidth() / img.getHeight() > img.getHeight() / img.getWidth()) {
             ratio = (double) img.getWidth() / (double) img.getHeight();
@@ -47,33 +46,15 @@ public class TextGraphicsConverterImpl implements TextGraphicsConverter {
 
         if (ratio > maxRatio && maxRatio != 0) throw new BadImageSizeException(ratio, maxRatio);
 
-//        if (img.getWidth() > width || img.getHeight() > height) {
-//            if (width != 0) {
-//                widthToChange = img.getWidth() / width;
-//            } else widthToChange = 1;
-//            if (height != 0) {
-//                heightToChange = img.getHeight() / height;
-//            } else heightToChange = 1;
-//
-//            if (widthToChange > heightToChange) {
-//                newWidth = (int) (img.getWidth() / widthToChange);
-//                newHeight = (int) (img.getHeight() / widthToChange);
-//            } else {
-//                newWidth = (int) (img.getWidth() / heightToChange);
-//                newHeight = (int) (img.getHeight() / heightToChange);
-//            }
-//        } else {
-//            newWidth = img.getWidth();
-//            newHeight = img.getHeight();
-//        }
-
         Image scaledImage = img.getScaledInstance(newWidth, newHeight, BufferedImage.SCALE_SMOOTH);
         BufferedImage bwImg = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_BYTE_GRAY);
         Graphics2D graphics = bwImg.createGraphics();
         graphics.drawImage(scaledImage, 0, 0, null);
 //        ImageIO.write((RenderedImage) scaledImage, "png", new File("out.png"));
         var bwRaster = bwImg.getRaster();
+
         char[][] newpic = new char[newHeight][newWidth];
+
         for (int i = 0; i < newHeight; i++) {
             for (int j = 0; j < newWidth; j++) {
                 int color = bwRaster.getPixel(j, i, new int[3])[0];
@@ -82,18 +63,16 @@ public class TextGraphicsConverterImpl implements TextGraphicsConverter {
             }
         }
 
-//        StringBuilder sb = new StringBuilder();
-//        for (int i = 0; i < newpic.length; i++) {
-//            for (int j = 0; j < newpic[i].length; j++){
-//                sb.append(newpic[i][j]);
-//                sb.append(newpic[i][j]);
-//            }
-//            sb.append("\n");
-//        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < newpic.length; i++) {
+            for (int j = 0; j < newpic[i].length; j++) {
+                sb.append(newpic[i][j]);
+                sb.append(newpic[i][j]);
+            }
+            sb.append("\n");
+        }
 
-        printNewPicture(newpic);
-        return newpic.toString();
-
+        return sb.toString();
     }
 
     public TextGraphicsConverterImpl() {
@@ -121,3 +100,4 @@ public class TextGraphicsConverterImpl implements TextGraphicsConverter {
     }
 
 }
+
